a4f2105cc7d0c4676559aac60d9eb468c8bfa906
idlakshan <id.lakshan21@gmail.com>
2022-07-28 11:34:37 +0530

    Create Register Interface

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,5 +1,30 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
 
+    public AnchorPane root;
+
+
+    public void lblCreateNewAccountOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        Parent parent= FXMLLoader.load(this.getClass().getResource("../view/CreateNewAccountForm.fxml"));
+
+        Scene scene=new Scene(parent);
+
+        Stage primaryStage= (Stage) root.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Register");
+
+
+
+    }
 }
