fc878367adca8e8b9e457520261004e66c04d519
idlakshan <id.lakshan21@gmail.com>
2022-08-13 12:49:42 +0530

    Item form done

--- src/controller/ItemFormController.java
+++ src/controller/ItemFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import db.DBConnection;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -41,6 +43,18 @@ public class ItemFormController {
 
 
         loadAllItems();
+
+        tblItem.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ItemTM>() {
+            @Override
+            public void changed(ObservableValue<? extends ItemTM> observable, ItemTM oldValue, ItemTM newValue) {
+                txtCode.setText(tblItem.getSelectionModel().getSelectedItem().getCode());
+                txtName.setText(tblItem.getSelectionModel().getSelectedItem().getName());
+                txtQtyOnHand.setText(String.valueOf(tblItem.getSelectionModel().getSelectedItem().getQty()));
+                txtQtyPrice.setText(String.valueOf(tblItem.getSelectionModel().getSelectedItem().getPrice()));
+            }
+
+        });
+
     }
 
     public void btnSaveItemOnAction(ActionEvent event) throws SQLException {
@@ -48,19 +62,66 @@ public class ItemFormController {
 
         if (saveItem(item)) {
             new Alert(Alert.AlertType.INFORMATION, "Saved..").show();
+            loadAllItems();
 
         } else {
             new Alert(Alert.AlertType.WARNING, "Try again.").show();
         }
     }
 
-    public void btnSearchItemOnAction(ActionEvent event) {
+    public void btnSearchItemOnAction(ActionEvent event) throws SQLException {
+        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement("select * from item where code=?");
+        statement.setObject(1, txtCode.getText());
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+
+            Item item = new Item(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getDouble(4)
+            );
+            searchItem(item);
+
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Empty results").show();
+        }
+
+
     }
 
-    public void btnUpdateItemOnAction(ActionEvent event) {
+    public void btnUpdateItemOnAction(ActionEvent event) throws SQLException {
+
+        Item item = new Item(
+                txtCode.getText(), txtName.getText(), Integer.parseInt(txtQtyOnHand.getText()), Double.parseDouble(txtQtyPrice.getText())
+
+        );
+
+
+        if (updateItem(item)) {
+            new Alert(Alert.AlertType.INFORMATION, "Updated").show();
+            loadAllItems();
+            clearTextFields();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try Again").show();
+        }
+
+
     }
 
-    public void btnDeleteItemOnAction(ActionEvent event) {
+    public void btnDeleteItemOnAction(ActionEvent event) throws SQLException {
+        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement("delete from item where code=?");
+        statement.setObject(1, txtCode.getText());
+
+        if (statement.executeUpdate() > 0) {
+            new Alert(Alert.AlertType.INFORMATION, "Deleted").show();
+            loadAllItems();
+            clearTextFields();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try again").show();
+        }
 
     }
 
@@ -87,12 +148,38 @@ public class ItemFormController {
                     resultSet.getString(2),
                     resultSet.getInt(3),
                     resultSet.getDouble(4)
-                  /*  resultSet.getString(3),
-                    resultSet.getString(4)*/
 
             ));
         }
         tblItem.setItems(observableList);
+    }
+
+    void searchItem(Item i) {
+        txtCode.setText(i.getCode());
+        txtName.setText(i.getName());
+        txtQtyOnHand.setText(String.valueOf(i.getQty()));
+        txtQtyPrice.setText(String.valueOf(i.getPrice()));
+
+    }
+
+    boolean updateItem(Item i) throws SQLException {
+        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement("update item set name=?,qtyOnhand=?,unitPrice=? where code=?");
+        statement.setObject(1, i.getName());
+        statement.setObject(2, i.getQty());
+        statement.setObject(3, i.getPrice());
+        statement.setObject(4, i.getCode());
+
+        return statement.executeUpdate() > 0;
+
+    }
+
+    public void clearTextFields() {
+        txtCode.clear();
+        txtName.clear();
+        txtQtyOnHand.clear();
+        txtQtyPrice.clear();
+
+        txtCode.requestFocus();
     }
 
 
